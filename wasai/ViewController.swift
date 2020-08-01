f44d4aa9795d47c63026704a616b86337d9f6722
smallwebbird <1175543548@qq.com>
2020-08-01 13:51:20 +0800

    Initial Commit

---
+++ wasai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wasai
+//
+//  Created by 李政辉 on 2020/8/1.
+//  Copyright © 2020 李政辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ wasaiTests/wasaiTests.swift
@@ -0,0 +1,34 @@
+//
+//  wasaiTests.swift
+//  wasaiTests
+//
+//  Created by 李政辉 on 2020/8/1.
+//  Copyright © 2020 李政辉. All rights reserved.
+//
+
+import XCTest
+@testable import wasai
+
+class wasaiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
